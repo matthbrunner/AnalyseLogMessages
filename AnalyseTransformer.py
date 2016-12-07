68e00dadc07f1fb9ec51c2c42105d18cfcf3107c
Matthias Brunner <matthias.brunner@geocom.ch>
2016-12-07 21:11:08 +0100

    Done method to search for a specific element and replace it with something different

--- AnalyseTransformer.py
+++ AnalyseTransformer.py
@@ -30,13 +30,27 @@ class AnalyseTransformer:
         return result_list
 
     # ---------------------------------------------------------------------------
-    def replace_hash_with_format(self, line):
-        elements = line.split('#')
-        print(elements)
+    def replace_hash_with_format(self, line, character):
+        dict = {}
+        language_expression = [pos for pos, char in enumerate(line) if char == character]
+
+        if len(language_expression) % 2 != 0:
+            logging.error("odd number of char {} in message {}".format(character, line))
+        else:
+            for i in range(0, len(language_expression) - 1, 2):
+                test = line[language_expression[i] + 1: language_expression[i + 1]]
+                to_replace = '{}{:d}{}'.format(str('{'), int(test[:test.index('|')]), str('}'))
+                dict[line[language_expression[i]: language_expression[i + 1] + 1]] = to_replace
+
+            for key, value in dict.items():
+                line = line.replace(str(key), str(value))
+
+        return dict
 
     # ---------------------------------------------------------------------------
     def read_all_lines_from_transformer(self, file, file_name):
         result_list = []
+        result_dict = {}
 
         # Open a file
         line = ""
@@ -46,7 +60,6 @@ class AnalyseTransformer:
                 if r'XFORM_PARM PARM_NAME="MESSAGE_TEXT"' in line:
                     tmp_line = line.replace(r'#! <XFORM_PARM PARM_NAME="MESSAGE_TEXT" PARM_VALUE="', '')
                     tmp_line = tmp_line.replace(r'"/>', '')
-                    # lng_line = replace_hash_with_format(tmp_line)
                     result_list.append("{0}; {1}; {2}".format(os.path.basename(file_name), tmp_line[:4], tmp_line[5:]))
 
         except Exception as e:
@@ -109,3 +122,6 @@ if __name__ == '__main__':
 
     if len(messages) > 0:
         at.write_all_messages_from_transformer(os.path.join(transformer_path, "test.csv"), messages)
+
+    # TODO: include methode [replace_hash_with_format]
+    # TODO: Add method to replace the language file if it's different
